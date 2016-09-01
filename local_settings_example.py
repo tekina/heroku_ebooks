238ddc16ac4c07f8346e65f816f12142c9dacf26
Aniket Garg <aniketgarg@outlook.com>
2016-09-01 15:32:25 +0530

    settings tweak

--- local_settings_example.py
+++ local_settings_example.py
@@ -3,15 +3,15 @@ Local Settings for a heroku_ebooks account. #fill in the name of the account you
 '''
 
 #configuration
-MY_CONSUMER_KEY = 'Your Twitter API Consumer Key'
-MY_CONSUMER_SECRET = 'Your Consumer Secret Key'
-MY_ACCESS_TOKEN_KEY = 'Your Twitter API Access Token Key'
-MY_ACCESS_TOKEN_SECRET = 'Your Access Token Secret'
+MY_CONSUMER_KEY = 'T4sEgP3kYxybwEXZCWR18mLIQ'
+MY_CONSUMER_SECRET = 'krU07qXnj4uc70gOpn0o3CsDaPjdm3T7owshZgt36wNgAfhOaZ'
+MY_ACCESS_TOKEN_KEY = '524534295-paB2f56UV68r81V3P7DG8gVxE7vcAbiwZ6gJCRXU'
+MY_ACCESS_TOKEN_SECRET = 'Jt6BlmTDoKdKl0t7VJ8MbHUX7EhuImNx2LV4AodOobM87'
 
-SOURCE_ACCOUNTS = [""] #A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
-ODDS = 8 #How often do you want this to run? 1/8 times?
-ORDER = 2 #how closely do you want this to hew to sensical? 1 is low and 3 is high.
-DEBUG = True #Set this to False to start Tweeting live
+SOURCE_ACCOUNTS = ["SiliconHBO", "HouseOfCards", "Frank_Underwood", "BarackObama", "SeinfeldToday"] #A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
+ODDS = 0  #How often do you want this to run? 1/8 times?
+ORDER = 1 #how closely do you want this to hew to sensical? 1 is low and 3 is high.
+DEBUG = False #Set this to False to start Tweeting live
 STATIC_TEST = False #Set this to True if you want to test Markov generation from a static file instead of the API.
 TEST_SOURCE = ".txt" #The name of a text file of a string-ified list for testing. To avoid unnecessarily hitting Twitter API.
-TWEET_ACCOUNT = "" #The name of the account you're tweeting to.
+TWEET_ACCOUNT = "tekina_musings" #The name of the account you're tweeting to.
